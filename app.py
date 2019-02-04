ec17028d572f2d19d9eb5572aacbceadddf00a7d
violetina <schizoid69@gmail.com>
2019-02-04 07:18:44 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+import datetime
+import logging
+from flask_cors import CORS
+import connexion
+from connexion import NoContent
+from flask import Flask, request, render_template, session, redirect
+
+import orm
+
+db_session = None
+
+
+def get_Bays(limit, status=None):
+    q = db_session.query(orm.Bay)
+    if status:
+        q = q.filter(orm.Bay.status == status)
+    return [p.dump() for p in q][:limit]
+
+
+def get_Bay(bay_id):
+    bay = db_session.query(orm.Bay).filter(orm.Bay.disk_bay == bay_id).one_or_none()
+    return bay.dump() if bay is not None else ('Not found', 404)
+
+
+def put_Bay(bay_id, bay):
+    p = db_session.query(orm.Bay).filter(orm.Bay.disk_bay == bay_id).one_or_none()
+    bay['disk_bay'] = bay_id
+    if p is not None:
+        logging.info('Updating bay %s..', bay_id)
+        p.update(**bay)
+    else:
+        logging.info('Creating bay %s..', bay_id)
+        bay['created'] = datetime.datetime.utcnow()
+        db_session.add(orm.Bay(**bay))
+    db_session.commit()
+    return NoContent, (200 if p is not None else 201)
+
+
+def delete_Bay(bay_id):
+    bay = db_session.query(orm.Bay).filter(orm.Bay.disk_bay == bay_id).one_or_none()
+    if bay is not None:
+        logging.info('Deleting bay %s..', bay_id)
+        db_session.query(orm.Bay).filter(orm.Bay.disk_bay == bay_id).delete()
+        db_session.commit()
+        return NoContent, 204
+    else:
+        return NoContent, 404
+
+
+logging.basicConfig(level=logging.INFO)
+db_session = orm.init_db('sqlite:////tmp/db.sqlite') 
+app = connexion.FlaskApp(__name__)
+
+app.add_api('openapi.yaml')
+
+application = app.app
+
+import pandas as pd
+import sqlite3
+
+@app.route('/log')
+def html_table():
+
+
+
+    conn = sqlite3.connect("/tmp/db.sqlite")
+    df = pd.read_sql_query("select * from bays limit 5;", conn)
+    print(df)
+    return render_template('simple.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+
+
+
+
+@application.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
+if __name__ == '__main__':
+    CORS(app.app)
+    app.run(port=8081, use_reloader=False, threaded=False)
---
+++ orm.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, DateTime, String, create_engine, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+Base = declarative_base()
+
+
+class Bay(Base):
+    __tablename__ = 'bays'
+    #id = Column(String(20), primary_key=True)
+    disk_bay = Column(Integer(), primary_key=True)
+    OR_ID = Column(String(10))
+    status = Column(String(20))
+    created = Column(DateTime())
+
+    def update(self,  disk_bay=None, OR_ID=None, status=None, created=None):
+        if OR_ID is not None:
+            self.OR_ID = OR_ID
+        if status is not None:
+            self.status = status
+        if created is not None:
+            self.created = created
+
+    def dump(self):
+        return dict([(k, v) for k, v in vars(self).items() if not k.startswith('_')])
+
+
+def init_db(uri):
+    engine = create_engine(uri, convert_unicode=True, echo=True)
+    db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+    Base.query = db_session.query_property()
+    Base.metadata.create_all(bind=engine)
+    return db_session
